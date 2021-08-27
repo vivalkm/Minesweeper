63fb82b93c9348448e32ac846bddcad74c71961b
Lincoln Li <vivalkm@gmail.com>
2021-08-26 21:49:40 -0700

    Add valid? and neighbor methods to board class

--- board.rb
+++ board.rb
@@ -53,6 +53,27 @@ class Board
     def [](pos)
         @grid[pos[0]][pos[1]]
     end
+
+    def neighbor(pos)
+        neighbors = []
+        up_pos =  [pos[0] - 1, pos[1]]
+        neighbors << self[up_pos] if valid?(up_pos)
+
+        down_pos =  [pos[0] + 1, pos[1]]
+        neighbors << self[down_pos] if valid?(down_pos)
+
+        left_pos =  [pos[0], pos[1] - 1]
+        neighbors << self[left_pos] if valid?(left_pos)
+
+        right_pos =  [pos[0], pos[1] + 1]
+        neighbors << self[right_pos] if valid?(right_pos)
+
+        return neighbors
+    end
+
+    def valid?(pos)
+        pos.all? { |i| (0...@size).include?(i) }
+    end
 end
 
 if __FILE__ == $PROGRAM_NAME
